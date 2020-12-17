bf03372a61a0b3d2f17f156e503bdb3f0eef784b
krisi01 <chrisisbeast@abv.bg>
2020-12-17 06:43:55 -0800

    Finished betting buttons

--- gambling/src/Components/Roulette/Roulette.js
+++ gambling/src/Components/Roulette/Roulette.js
@@ -24,28 +24,47 @@ const data = [
  
   const [mustSpin, setMustSpin] = useState(false);
   const [prizeNumber, setPrizeNumber] = useState(null);
-    
-    let finalNum = null;
+  const [finalResult, setFinalResult] = useState({
+        number: null,
+        color: null
+  })
+    const [balance, setBalance] = useState(3000);
+    const [placeBet, setPlaceBet] = useState({
+        betSize: null,
+        betPlaced: false
+    })
+  const [payBets, setPayBets] = useState(false);
     const handleSpinClick = () => {
         const newPrizeNumber = Math.floor(Math.random() * data.length);
-        finalNum = newPrizeNumber;
         setPrizeNumber(newPrizeNumber)
         setMustSpin(true);
+        setFinalResult({...finalResult, number: data[newPrizeNumber].option, color: data[newPrizeNumber].style.backgroundColor});
     }
     
-   console.log(finalNum);
+    const updateBet = e => {
+        const newValue = Number(e.target.value);
+        setPlaceBet({...placeBet, betSize: newValue});
+        console.log(placeBet);
+    }
+    
   return (
-    <div className={classes.Roulette}>
+      <div className={classes.Roulette}>
+          {console.log(finalResult)}
       <Wheel
         mustStartSpinning={mustSpin}
         prizeNumber={prizeNumber}
         data={data}
  
         onStopSpinning={() => {
-          setMustSpin(false)
+            setMustSpin(false)
+            setPayBets(!payBets);
+            console.log(finalResult);
         }}
           />
-      <button onClick={handleSpinClick}>SPIN</button>
+         <div className={classes.D1}> <p><strong>Bet amount:</strong></p><input type='number' onChange={updateBet} defaultValue={placeBet.betSize}></input></div>
+          <div><strong>Balance: {balance}</strong></div>
+          <button className={classes.Button2}>Black</button> <button className={classes.Button1}>Red</button>
+      <button onClick={handleSpinClick} className={classes.Button}>SPIN</button>
      </div>
   )
     }
