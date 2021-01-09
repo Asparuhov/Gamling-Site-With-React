4e1936f3ca5e1bae39017c9159b523233c3595d7
krisi01 <chrisisbeast@abv.bg>
2021-01-09 23:36:41 +0200

    working on dummy chat system

--- gambling/src/Components/Chat/Chat.js
+++ gambling/src/Components/Chat/Chat.js
@@ -1,27 +1,43 @@
 import React, { useState } from "react";
 import classes from "./Chat.module.css";
 import Message from "./Message";
+import { useAuth0 } from "@auth0/auth0-react";
 const Chat = (props) => {
-  let [messages, setMessages] = useState({
-    currentUser: "",
-    message: "",
-    date: "",
-  });
-  const addMessage = () => {};
+  const { user } = useAuth0();
+  let [currentMessage, setCurrentMessage] = useState("");
+  let [messages, setMessages] = useState([]);
+  const addMessage = () => {
+    setMessages((oldArray) => [
+      ...oldArray,
+      {
+        currentUser: user.nickname,
+        message: currentMessage,
+      },
+    ]);
+    setCurrentMessage('');
+  };
   return (
     <div className={classes.Container}>
       <div className={classes.header}>
         <h2>Messages</h2>
-        <a href="/" title="Add Friend to this chat"></a>
       </div>
       <div className={classes.chatbox}>
-        <Message
-          user="krismata"
-          message="kvo pravish ti be"
-          date="22 january"
-        />
+        <div className={classes.content}>
+          {messages.map((message) => {
+            return (
+              <Message user={message.currentUser} message={message.message} />
+            );
+          })}
+        </div>
         <div className={classes.entermessage}>
-          <input type="text" placeholder="Enter your message.." />
+          <input
+            type="text"
+            placeholder="Enter your message.."
+            onChange={(e) => {
+              setCurrentMessage(e.target.value);
+            }}
+            value={currentMessage}
+          />
           <button class={classes.send} onClick={() => addMessage()}>
             Send
           </button>
